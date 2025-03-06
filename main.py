954578cb5b4c7e79e070fa86f45d043eab353954
arnb-smnta <loveumearnab.2812000@gmail.com>
2025-03-06 14:19:43 +0530

    fix main.py for render to render

--- main.py
+++ main.py
@@ -54,6 +54,5 @@ async def get_openapi_yaml():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(
-        "main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True
-    )
+    port = int(os.environ.get("PORT", 8000))  # Use Render's assigned port
+    uvicorn.run(app, host="0.0.0.0", port=port)
