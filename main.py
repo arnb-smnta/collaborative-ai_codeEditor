a171dda671dfa466ae0de339884847902c88cd32
arnb-smnta <loveumearnab.2812000@gmail.com>
2025-03-07 14:42:05 +0530

    feat:set up rate limiter globally

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from slowapi import Limiter
 from routes import users, files, collaboration, ai
 import os
 from db import engine, Base
 from fastapi.openapi.utils import get_openapi
 import yaml
+
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,6 +19,27 @@ app = FastAPI(
     description="A Fast-api powered real time ai editor with specific out of the world ai support for python and js",
     version="1.0.0",
 )
+limiter = Limiter(
+    key_func=get_remote_address,  # Uses client IP for rate limiting
+    default_limits=[
+        "500000/day",
+        "500/minute",
+    ],
+    storage_uri="memory://",  # Uses in-memory storage(ram) it will reset if the server resets it is for this app only have to use any other database in big productions
+)
+
+
+app.state.limiter = limiter
+app.add_middleware(SlowAPIMiddleware)
+
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_exceeded_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        content={"error": "Too many requests. Please try again later."},
+    )
+
 
 origins = os.getenv("CORS_ORIGIN", "*").split(",")
 
@@ -54,5 +80,5 @@ async def get_openapi_yaml():
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.environ.get("PORT", 8000))  # Use Render's assigned port
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
